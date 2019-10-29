83fafcc921402e9c78ee8c1b8a71e6bf35ed4b9e
Olga Zinoveva <ozinka81@gmail.com>
2019-10-29 20:38:37 +0100

    update model event

---
+++ routes/events.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const Event = require("../models/Event");
+const User = require("../models/User");
+const { checkIfLoggedIn } = require("../middlewares/index");
+
+const router = express.Router();
+
+//
+
+module.exports = router;
