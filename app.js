5b40655fe36c03c30177d6231076451a97375ada
Olga Zinoveva <ozinka81@gmail.com>
2019-10-31 19:46:03 +0100

    add new event route

--- app.js
+++ app.js
@@ -1,54 +1,58 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const cors = require('cors');
-require('dotenv').config();
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const cors = require("cors");
+require("dotenv").config();
 
-mongoose.set('useCreateIndex', true);
+mongoose.set("useCreateIndex", true);
 mongoose
-  .connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('connected to: ', process.env.MONGO_URL);
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
   })
-  .catch((error) => {
+  .then(() => {
+    console.log("connected to: ", process.env.MONGO_URL);
+  })
+  .catch(error => {
     console.error(error);
   });
 
-const authRouter = require('./routes/auth');
+const authRouter = require("./routes/auth");
+const eventsRouter = require("./routes/events");
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
   session({
     store: new MongoStore({
       mongooseConnection: mongoose.connection,
-      ttl: 24 * 60 * 60, // 1 day
+      ttl: 24 * 60 * 60 // 1 day
     }),
     secret: process.env.SECRET,
     resave: true,
     saveUninitialized: true,
     cookie: {
-      maxAge: 24 * 60 * 60 * 1000,
-    },
-  }),
+      maxAge: 24 * 60 * 60 * 1000
+    }
+  })
 );
 
 app.use(
   cors({
     credentials: true,
-    origin: [process.env.FRONTEND_URL],
-  }),
+    origin: [process.env.FRONTEND_URL]
+  })
 );
 
 app.use((req, res, next) => {
@@ -56,20 +60,21 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/', authRouter);
+app.use("/", authRouter);
+app.use("/api", eventsRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  res.status(404).json({ code: 'not found' });
+  res.status(404).json({ code: "not found" });
 });
 
 app.use((err, req, res, next) => {
   // always log the error
-  console.error('ERROR', req.method, req.path, err);
+  console.error("ERROR", req.method, req.path, err);
 
   // only render if the error ocurred before sending the response
   if (!res.headersSent) {
-    res.status(500).json({ code: 'unexpected' });
+    res.status(500).json({ code: "unexpected" });
   }
 });
 
--- routes/events.js
+++ routes/events.js
@@ -5,6 +5,48 @@ const { checkIfLoggedIn } = require("../middlewares/index");
 
 const router = express.Router();
 
-//
+/* GET event listing. */
+router.get("/events", checkIfLoggedIn, async (req, res, next) => {
+  try {
+    const event = await Event.find(day).populate("user");
+    res.json(event);
+  } catch (error) {
+    next(error);
+  }
+});
+
+/* Add/post event */
+// router.post("/events", checkIfLoggedIn, async (req, res, next) => {
+//   const { alcohol, money, quantity, health } = req.body;
+//   try {
+//     const event = await Event.create({
+//       alcohol,
+//       money,
+//       quantity,
+//       health
+//     });
+//     res.json(event);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
+router.post("/events", checkIfLoggedIn, async (req, res, next) => {
+  const { alcohol, money, quantity, health } = req.body;
+  const owner = req.session.currentUser._id;
+
+  try {
+    const event = await Event.create({
+      user: owner,
+      alcohol,
+      money,
+      quantity,
+      health
+    });
+    res.json(event);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
