0b6aa3dd853b1271e27aad9c6a8b41fddbfae6b1
Olga Zinoveva <ozinka81@gmail.com>
2019-11-04 23:23:04 +0100

    add drink model

--- models/Drink.js
+++ models/Drink.js
@@ -13,6 +13,30 @@ drinkTypesArray = [
   "Coctail"
 ];
 
+const drinkSchema = new Schema(
+  /* name: drink name ('Martini', 'Jameson', 'Heineken' etc.) needs to be specified by user at the time of drink creation	*/
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    /* type: more broad drink type category ("Beer","Wine",	"Cider", etc.) needs to be specified by user at the time of drink creation	*/
+    type: {
+      type: String,
+      enum: drinkTypesArray,
+      required: true
+    },
+    /* percentage: alcohol %% in the drink. needs to be specified by user at the time of drink creation	*/
+    percentage: {
+      type: Number,
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
 const Drink = mongoose.model("Drink", drinkSchema);
 
 module.exports = {
